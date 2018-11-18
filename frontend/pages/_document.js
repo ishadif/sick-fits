fa5a37aaae5646cc0cfd86f54c098491e65fbcfd
ishadif <ishadi.fauzan@hotmail.com>
2018-11-19 01:25:51 +0700

    react helmet experiment, sorry about that

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,21 +1,52 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import Helmet from 'react-helmet';
 
 export default class MyDocument extends Document {
   static getInitialProps ({ renderPage }) {
     const sheet = new ServerStyleSheet()
     const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
     const styleTags = sheet.getStyleElement()
-    return { ...page, styleTags }
+    return { ...page, styleTags, helmet: Helmet.renderStatic() }
+  }
+
+  get helmetHtmlAttrComponents () {
+    return this.props.helmet.htmlAttributes.toComponent()
+  }
+
+  // should render on <body>
+  get helmetBodyAttrComponents () {
+    return this.props.helmet.bodyAttributes.toComponent()
+  }
+
+  // should render on <head>
+  get helmetHeadComponents () {
+    return Object.keys(this.props.helmet)
+      .filter(el => el !== 'htmlAttributes' && el !== 'bodyAttributes')
+      .map(el => this.props.helmet[el].toComponent())
+  }
+
+  get helmetJsx () {
+    return (<Helmet
+      htmlAttributes={{lang: 'en'}}
+      title='Hello next.js!'
+      meta={[
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { property: 'og:title', content: 'Hello next.js!' },
+        {name: 'description', content: 'this is SSR!'}
+      ]}
+    />)
   }
 
   render () {
     return (
-      <html>
+      <html {...this.helmetHtmlAttrComponents}>
         <Head>
+          { this.helmetJsx }
+          { this.helmetHeadComponents }
           {this.props.styleTags}
         </Head>
-        <body>
+        <body {...this.helmetBodyAttrComponents}>
           <Main />
           <NextScript />
         </body>
